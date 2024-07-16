9eced5681785470cd0540b7ab9d0c2f4e749d310
siveryt <github@sivery.de>
2024-07-16 11:21:51 +0200

    Added Serverstatus indicator

--- public/index.js
+++ public/index.js
@@ -7,3 +7,22 @@ document.getElementById('random').addEventListener('click', function() {
     let roomcode = Math.random().toString(36).substring(7);
     window.location = `${window.location.origin}/${roomcode}`;
 });
+
+const statusElement = document.getElementById('serverstatus');
+
+const https = document.location.protocol === 'https:';
+const ws = new WebSocket(`ws${https ? 's' : ''}://${window.location.host}`)
+
+ws.onopen = function() {
+    console.log('Connected to server');
+    statusElement.innerText = 'Connected to Server';
+    statusElement.classList.remove('is-danger');
+    statusElement.classList.add('is-success');
+}
+
+ws.onclose = function() {
+    console.log('Disconnected from server');
+    statusElement.innerText = 'No Connection';
+    statusElement.classList.remove('is-success');
+    statusElement.classList.add('is-danger');
+}
